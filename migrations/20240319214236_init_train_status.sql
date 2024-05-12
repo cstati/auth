598e25aaa0fecce6101c9a95c8056f3e3d1e7e24
ltulyavko <wot.k@mail.ru>
2024-05-13 02:18:17 +0300

    new schemas

--- migrations/20240319214236_init_train_status.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-CREATE TYPE model_training_status AS ENUM ('not_started', 'in_progress', 'error', 'done');
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-drop type if exists model_training_status;
--- +goose StatementEnd
--- migrations/20240321100337_init_launches_enum.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-create type launch_type as enum ('train', 'predict', 'generic');
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-drop type launch_type;
--- +goose StatementEnd
